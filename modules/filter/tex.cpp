83356ed264162677be6a8f3b4b3c3f3ea60c3321
Kevin Atkinson <kevina@gnu.org>
2024-03-31 12:51:00 -0400

    Checkpoint.  FIXME: Update commit.

--- modules/filter/tex.cpp
+++ modules/filter/tex.cpp
@@ -1,8 +1,10 @@
 // This file is part of The New Aspell
-// Copyright (C) 2001 by Kevin Atkinson under the GNU LGPL license
-// version 2.0 or 2.1.  You should have received a copy of the LGPL
-// license along with this library if you did not you can find
-// it at http://www.gnu.org/.
+// Copyright (C) 2001, 2016 Kevin Atkinson under the GNU LGPL license
+// version 2.0 or 2.1.
+// Copyright (C) 2011, 2022 Matthias Franz under the GNU LGPL license
+// version 2.1 or (at your option) any later version.
+// You should have received a copy of the LGPL license along with this library
+// if you did not you can find it at http://www.gnu.org/.
 
 #include "asc_ctype.hpp"
 #include "config.hpp"
