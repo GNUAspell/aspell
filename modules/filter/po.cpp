78b7e5e9c41278488fc0fb532df3869e89205c1e
Igor Támara <igor.tamara@gmail.com>
2025-06-17 17:35:02 -0500

    Specified year for licensing

--- modules/filter/po.cpp
+++ modules/filter/po.cpp
@@ -1,8 +1,9 @@
 // This file is part of The New Aspell
-// Copyright (C) 2025 by Kevin Atkinson and Igor Támara under the
-// GNU LGPL license version 2.0 or 2.1.  You should have received
-// a copy of the LGPL license along with this library if you did
-// not you can find it at http://www.gnu.org/.
+// Copyright (C) 2019 by Kevin Atkinson  and
+// Copyright (C) 2025 by Igor Támara
+// under the GNU LGPL license version 2.0 or 2.1.  You should
+// have received a copy of the LGPL license along with this
+// library if you did not you can find it at http://www.gnu.org/.
 
 #include "settings.h"
 
